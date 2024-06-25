6b06cfc2cca15facf167fd2e92be0060d14e8917
pamu <pamu1976@outlook.com>
2024-06-25 22:05:30 +0800

    3.3.0版本通过Component 和 ConfigurationProperties 获取配置，获取不到

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,15 @@
 package com.example.demo;
 
+import com.example.demo.bean.Person;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DemoApplicationTests {
-
+	private Person person;
 	@Test
 	void contextLoads() {
+		System.out.println(person);
 	}
 
 }
