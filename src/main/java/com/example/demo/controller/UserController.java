63fc1ab736c78fc17276bb09f4365996f9cd9fc4
pamu <pamu1976@outlook.com>
2024-06-25 22:39:51 +0800

    通过注解添加一个restful api

---
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,14 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+    @GetMapping("/hello")
+    public String getHello() {
+        return "Hello from Spring Boot!";
+    }
+}
