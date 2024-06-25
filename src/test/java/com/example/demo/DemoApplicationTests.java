1c3971eaaa875140ac0d8533083b5eb66ca1c0e3
pamu <pamu1976@outlook.com>
2024-06-25 22:23:16 +0800

    @Value注解或Environment对象来获取application.yml中的配置信息

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,15 +1,23 @@
 package com.example.demo;
 
-import com.example.demo.bean.Person;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.env.Environment;
 
 @SpringBootTest
 class DemoApplicationTests {
-	private Person person;
+	@Value("${person.name}")
+	private String name;
+
+	@Autowired
+	private Environment env;
+
 	@Test
 	void contextLoads() {
-		System.out.println(person);
+		System.out.println("通过@Value注解获取配置信息"+name);
+		System.out.println(env.getProperty("person.age"));
 	}
 
 }
